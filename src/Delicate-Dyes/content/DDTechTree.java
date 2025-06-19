5afba15d4867cd61b0f010a2f455ad68a4036aaa
Raviravirego76466 <raviravirego@gmail.com>
2025-06-19 18:22:47 -0300

    Create DDTechTree.java

---
+++ src/Delicate-Dyes/content/DDTechTree.java
@@ -0,0 +1,20 @@
+package Delicate-Dyes.content;
+
+import arc.struct.*;
+import mindustry.Vars;
+import mindustry.content.*;
+import mindustry.ctype.*;
+import mindustry.game.Objectives.*;
+import mindustry.type.*;
+
+import static mindustry.content.Blocks.*;
+import static mindustry.content.TechTree.*;
+import static Delicate-Dyes.content.DDBlocks*;
+
+public class EUTechTree {
+    public static TechNode context = null;
+
+    public static void load() {
+        addToNode(sporePress, () -> node(blueExtractor));
+        addToNode(blueExtractor, () -> node(BurplePress));
+}}
