a19220df7c5bba9b2d11c02ac8b051e88b666cdc
Raviravirego76466 <raviravirego@gmail.com>
2025-06-19 17:14:28 -0300

    Create DDItems.java

---
+++ src/Delicate-Dyes/content/DDItems.java
@@ -0,0 +1,11 @@
+package Delicate-Dyes.content;
+
+import arc.graphics.Color;
+import mindustry.graphics.Pal;
+import mindustry.type.Item;
+
+public class DDItems {
+    public static Item Blueberries = new Item("blueberries", Color.valueOf("3a3780")){{
+        cost = 1f;
+    }};
+}
